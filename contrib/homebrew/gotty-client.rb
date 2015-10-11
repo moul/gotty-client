414dbae3ce9793d88681ac6cf9b3d45f5a659cf4
Manfred Touron <m@42.am>
2015-10-11 08:56:10 +0200

    Bump homebrew to v1.1.0

--- contrib/homebrew/gotty-client.rb
+++ contrib/homebrew/gotty-client.rb
@@ -3,8 +3,8 @@ require "language/go"
 class GottyClient < Formula
   desc "gotty-client: GoTTY client for your terminal"
   homepage "https://github.com/moul/gotty-client"
-  url "https://github.com/moul/gotty-client/archive/v1.0.1.tar.gz"
-  sha256 "b74a2501218863b3853dc871f53dab1893357dfcd395be853c0f36b3d427928c"
+  url "https://github.com/moul/gotty-client/archive/v1.1.0.tar.gz"
+  sha256 "f080e685d6eaff9dfe627d8a7b5b485e2d023fa1ec0b1db7db37775177008708"
 
   head "https://github.com/moul/gotty-client.git"
 
@@ -19,7 +19,7 @@ class GottyClient < Formula
     ln_s buildpath, buildpath/"src/github.com/moul/gotty-client"
     Language::Go.stage_deps resources, buildpath/"src"
 
-    # FIXME: update version
+    # FIXME: update version variable
     system "go", "build", "-o", "gotty-client", "./cmd/gotty-client/"
     bin.install "gotty-client"
 
@@ -27,8 +27,7 @@ class GottyClient < Formula
   end
 
   test do
-    # FIXME: add test
-    #output = shell_output(bin/"gotty-client --version")
-    #assert output.include? "gotty-client version XXX"
+    output = shell_output(bin/"gotty-client --version")
+    assert output.include? "gotty-client version"
   end
 end
