25db7ca9f57920991e73c42d4a929153e66d80b3
Manfred Touron <m@42.am>
2016-01-04 12:27:51 +0100

    godep-save

--- vendor/golang.org/x/crypto/ssh/terminal/util.go
+++ vendor/golang.org/x/crypto/ssh/terminal/util.go
@@ -14,7 +14,7 @@
 // 	        panic(err)
 // 	}
 // 	defer terminal.Restore(0, oldState)
-package terminal // import "github.com/moul/gotty-client/vendor/golang.org/x/crypto/ssh/terminal"
+package terminal
 
 import (
 	"io"
