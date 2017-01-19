fd2d4f12dcd73cec8f89d27604535c3209af17cc
Manfred Touron <m@42.am>
2017-01-19 05:46:18 +0100

    Bump homebrew@1.6.1

--- contrib/homebrew/gotty-client.rb
+++ contrib/homebrew/gotty-client.rb
@@ -3,8 +3,8 @@ require "language/go"
 class GottyClient < Formula
   desc "gotty-client: GoTTY client for your terminal"
   homepage "https://github.com/moul/gotty-client"
-  url "https://github.com/moul/gotty-client/archive/v1.6.0.tar.gz"
-  sha256 "5f8b67fd47c1586a06aead9a89706d49c52cd4aaaad9181e38722d4be6a78d43"
+  url "https://github.com/moul/gotty-client/archive/v1.6.1.tar.gz"
+  sha256 "322aba97334073eaac833f1b40ba83c7861ba32247d3160504cc78f64b5a2ef6"
 
   head "https://github.com/moul/gotty-client.git"
 
